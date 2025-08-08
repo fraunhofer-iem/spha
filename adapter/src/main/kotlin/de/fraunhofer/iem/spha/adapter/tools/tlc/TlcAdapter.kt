6f3ad80cee2dfbf5a969e700d2c8b1e2d101f1fc
Jan-Niclas Strüwer <j.n.struewer@gmail.com>
2025-08-08 14:13:48 +0200

    Feat/tlc (#68)
    * refactor: updated TLC adapter. remove unused model classes and associated references
    
    * refactor: removed unused model files. updated tests
    
    * refactor: removed obsolete tests
    * refactor: removed obsolete tests
    
    * chore: bumped gradlew to 9.0.0
    
    * chore: version bumps

--- adapter/src/main/kotlin/de/fraunhofer/iem/spha/adapter/tools/tlc/TlcAdapter.kt
+++ adapter/src/main/kotlin/de/fraunhofer/iem/spha/adapter/tools/tlc/TlcAdapter.kt
@@ -10,15 +10,9 @@
 package de.fraunhofer.iem.spha.adapter.tools.tlc
 
 import de.fraunhofer.iem.spha.adapter.AdapterResult
-import de.fraunhofer.iem.spha.adapter.ErrorType
 import de.fraunhofer.iem.spha.adapter.KpiAdapter
-import de.fraunhofer.iem.spha.adapter.tools.tlc.model.Project
-import de.fraunhofer.iem.spha.adapter.tools.tlc.model.Version
-import de.fraunhofer.iem.spha.adapter.tools.tlc.util.TechLagHelper.getTechLagForGraph
-import de.fraunhofer.iem.spha.model.adapter.TlcConfig
-import de.fraunhofer.iem.spha.model.adapter.TlcDefaultConfig
+import de.fraunhofer.iem.spha.model.adapter.Tlc
 import de.fraunhofer.iem.spha.model.adapter.TlcDto
-import de.fraunhofer.iem.spha.model.adapter.TlcOriginDto
 import de.fraunhofer.iem.spha.model.kpi.KpiType
 import de.fraunhofer.iem.spha.model.kpi.RawValueKpi
 
@@ -28,73 +22,125 @@ sealed class TechLagResult {
     data class Empty(val reason: String) : TechLagResult()
 }
 
-object TlcAdapter : KpiAdapter<TlcDto, TlcOriginDto>() {
+object TlcAdapter : KpiAdapter<TlcDto, Tlc>() {
 
-    var config: TlcConfig = TlcDefaultConfig.get()
-
-    override fun transformDataToKpi(vararg data: TlcDto): Collection<AdapterResult<TlcOriginDto>> {
+    override fun transformDataToKpi(vararg data: TlcDto): Collection<AdapterResult<Tlc>> {
 
         return data.flatMap { tlcDto ->
-            tlcDto.projectDtos.flatMap { projectDto ->
-                val project = Project.from(projectDto)
-                project.graph.map { (scope, graph) ->
-                    val techLag =
-                        getTechLagForGraph(
-                            graph = graph,
-                            artifacts = project.artifacts,
-                            targetVersion = Version.Major,
-                        )
-
-                    if (techLag is TechLagResult.Success) {
-
-                        val libyearScore = getLibyearScore(techLag.libyear, config)
-
-                        val rawValueKpi =
-                            if (isProductionScope(ecosystem = project.ecosystem, scope = scope)) {
-                                RawValueKpi(
-                                    score = libyearScore,
-                                    typeId = KpiType.LIB_DAYS_PROD.name,
-                                )
-                            } else {
-                                RawValueKpi(
-                                    score = libyearScore,
-                                    typeId = KpiType.LIB_DAYS_DEV.name,
-                                )
-                            }
-
-                        return@map AdapterResult.Success.Kpi(
-                            rawValueKpi = rawValueKpi,
-                            origin = TlcOriginDto(projectDto, techLag.libyear),
-                        )
-                    }
-
-                    return@map AdapterResult.Error(ErrorType.DATA_VALIDATION_ERROR)
-                }
-            }
-        }
-    }
-
-    private fun getLibyearScore(libyear: Long, config: TlcConfig): Int {
-
-        if (libyear < 0L) {
-            return 100
-        }
-
-        val sortedThresholds = config.thresholds.sortedBy { it.range.from }
-
-        sortedThresholds.forEach { threshold ->
-            if (libyear > threshold.range.from && libyear < threshold.range.to) {
-                return threshold.score
-            }
-        }
-
-        return 0
-    }
-
-    private fun isProductionScope(scope: String, ecosystem: String): Boolean {
-        return when (ecosystem) {
-            "NPM" -> "dependencies" == scope
-            else -> true
+            listOf(
+                AdapterResult.Success.Kpi(
+                    RawValueKpi(
+                        typeId = KpiType.LIB_DAYS_DEV.name,
+                        score = tlcDto.optional.libdays.toInt(),
+                    ),
+                    tlcDto.optional,
+                ),
+                AdapterResult.Success.Kpi(
+                    RawValueKpi(
+                        typeId = KpiType.LIB_DAYS_PROD.name,
+                        score = tlcDto.production.libdays.toInt(),
+                    ),
+                    tlcDto.production,
+                ),
+                AdapterResult.Success.Kpi(
+                    RawValueKpi(
+                        typeId = KpiType.LIB_DAYS_DIRECT_DEV.name,
+                        score = tlcDto.directOptional.libdays.toInt(),
+                    ),
+                    tlcDto.directOptional,
+                ),
+                AdapterResult.Success.Kpi(
+                    RawValueKpi(
+                        typeId = KpiType.LIB_DAYS_DIRECT_PROD.name,
+                        score = tlcDto.directProduction.libdays.toInt(),
+                    ),
+                    tlcDto.directProduction,
+                ),
+                AdapterResult.Success.Kpi(
+                    RawValueKpi(
+                        typeId = KpiType.LIB_DAYS_MISSED_RELEASES_DEV.name,
+                        score = tlcDto.optional.missedReleases,
+                    ),
+                    tlcDto.optional,
+                ),
+                AdapterResult.Success.Kpi(
+                    RawValueKpi(
+                        typeId = KpiType.LIB_DAYS_MISSED_RELEASES_PROD.name,
+                        score = tlcDto.production.missedReleases,
+                    ),
+                    tlcDto.production,
+                ),
+                AdapterResult.Success.Kpi(
+                    RawValueKpi(
+                        typeId = KpiType.LIB_DAYS_MISSED_RELEASES_DIRECT_DEV.name,
+                        score = tlcDto.directOptional.missedReleases,
+                    ),
+                    tlcDto.directOptional,
+                ),
+                AdapterResult.Success.Kpi(
+                    RawValueKpi(
+                        typeId = KpiType.LIB_DAYS_MISSED_RELEASES_DIRECT_PROD.name,
+                        score = tlcDto.directProduction.missedReleases,
+                    ),
+                    tlcDto.directProduction,
+                ),
+                AdapterResult.Success.Kpi(
+                    RawValueKpi(
+                        typeId = KpiType.HIGHEST_LIB_DAYS_DEV.name,
+                        score = tlcDto.optional.highestLibdays.toInt(),
+                    ),
+                    tlcDto.optional,
+                ),
+                AdapterResult.Success.Kpi(
+                    RawValueKpi(
+                        typeId = KpiType.HIGHEST_LIB_DAYS_DIRECT_DEV.name,
+                        score = tlcDto.directOptional.highestLibdays.toInt(),
+                    ),
+                    tlcDto.directOptional,
+                ),
+                AdapterResult.Success.Kpi(
+                    RawValueKpi(
+                        typeId = KpiType.HIGHEST_LIB_DAYS_PROD.name,
+                        score = tlcDto.production.highestLibdays.toInt(),
+                    ),
+                    tlcDto.production,
+                ),
+                AdapterResult.Success.Kpi(
+                    RawValueKpi(
+                        typeId = KpiType.HIGHEST_LIB_DAYS_DIRECT_PROD.name,
+                        score = tlcDto.directProduction.highestLibdays.toInt(),
+                    ),
+                    tlcDto.directProduction,
+                ),
+                AdapterResult.Success.Kpi(
+                    RawValueKpi(
+                        typeId = KpiType.HIGHEST_LIB_DAYS_MISSED_RELEASES_DEV.name,
+                        score = tlcDto.optional.highestMissedReleases,
+                    ),
+                    tlcDto.optional,
+                ),
+                AdapterResult.Success.Kpi(
+                    RawValueKpi(
+                        typeId = KpiType.HIGHEST_LIB_DAYS_MISSED_RELEASES_PROD.name,
+                        score = tlcDto.production.highestMissedReleases,
+                    ),
+                    tlcDto.production,
+                ),
+                AdapterResult.Success.Kpi(
+                    RawValueKpi(
+                        typeId = KpiType.HIGHEST_LIB_DAYS_MISSED_RELEASES_DIRECT_DEV.name,
+                        score = tlcDto.directOptional.highestMissedReleases,
+                    ),
+                    tlcDto.directOptional,
+                ),
+                AdapterResult.Success.Kpi(
+                    RawValueKpi(
+                        typeId = KpiType.HIGHEST_LIB_DAYS_MISSED_RELEASES_DIRECT_PROD.name,
+                        score = tlcDto.directProduction.highestMissedReleases,
+                    ),
+                    tlcDto.directProduction,
+                ),
+            )
         }
     }
 }

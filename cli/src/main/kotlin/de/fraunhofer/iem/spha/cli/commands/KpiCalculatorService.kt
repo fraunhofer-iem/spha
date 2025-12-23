5fa0469b07853908be62043d3dc5fc5ca3451f35
Jan-Niclas Struewer <jan-niclas.struewer@iem.fraunhofer.de>
2025-12-23 11:32:00 +0100

    chore: codestyle

--- cli/src/main/kotlin/de/fraunhofer/iem/spha/cli/commands/KpiCalculatorService.kt
+++ cli/src/main/kotlin/de/fraunhofer/iem/spha/cli/commands/KpiCalculatorService.kt
@@ -1,9 +1,9 @@
 /*
  * Copyright (c) 2024-2025 Fraunhofer IEM. All rights reserved.
  *
- * Licensed under the MIT license. See LICENSE file in the project root for details. 
+ * Licensed under the MIT license. See LICENSE file in the project root for details.
  *
- * SPDX-License-Identifier: MIT 
+ * SPDX-License-Identifier: MIT
  * License-Filename: LICENSE
  */
 

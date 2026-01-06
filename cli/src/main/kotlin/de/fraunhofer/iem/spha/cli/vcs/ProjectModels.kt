1a411deacb631b8d40691d51296e5b57f1da827c
AnakinRaW <8994294+AnakinRaW@users.noreply.github.com>
2026-01-06 12:39:42 +0100

    feat: add support for project information fetching and modeling
    - Introduced `NetworkResponse` sealed class for success and failure handling.
    - Added `ProjectInfoFetcher` interface to fetch repository details.
    - Defined `ProjectInfo` and `Language` models for project metadata representation.

---
+++ cli/src/main/kotlin/de/fraunhofer/iem/spha/cli/vcs/ProjectModels.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2025 Fraunhofer IEM. All rights reserved.
+ *
+ * Licensed under the MIT license. See LICENSE file in the project root for details.
+ *
+ * SPDX-License-Identifier: MIT
+ * License-Filename: LICENSE
+ */
+
+package de.fraunhofer.iem.spha.cli.vcs
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ProjectInfo(
+    val name: String,
+    val usedLanguages: List<Language>,
+    val url: String,
+    val stars: Int,
+    val numberOfContributors: Int,
+    val numberOfCommits: Int?,
+    val lastCommitDate: String?,
+)
+
+@Serializable 
+data class Language(val name: String, val size: Int)

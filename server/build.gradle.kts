805e5c11479ad87ea59defbec0f08a0251a68ce1
Jan-Niclas Struewer <jan-niclas.struewer@iem.fraunhofer.de>
2026-01-09 14:14:01 +0100

    feat: add Ktor WebSocket support to project dependencies
    - Added `ktor-server-websocket` to `libs.versions.toml`.
    - Included WebSocket dependency in `server/build.gradle.kts` for implementation.
    
    Signed-off-by: Jan-Niclas Struewer <jan-niclas.struewer@iem.fraunhofer.de>

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.auth.jwt)
     implementation(libs.ktor.server.request.validation)
+    implementation(libs.ktor.server.websocket)
     implementation(libs.http.ktor.serialization)
     implementation(libs.ktor.server.content.negotiation)
     implementation(libs.postgresql)
